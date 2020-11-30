e53b97e83f9cfacf09321d2c692be1bcf16f7a8b
srikanthpragada <srikanthpragada@gmail.com>
2020-11-30 08:57:18 +0530

    30-NOV

--- demo/oop/account.py
+++ demo/oop/account.py
@@ -1,3 +1,11 @@
+class InsufficientBalanceError(Exception):
+    def __init__(self, balance):
+        self.balance = balance
+
+    def __str__(self):
+        return f"Insufficient Balance. Available balance is {self.balance}"
+
+
 class SavingsAccount:
     # Static variable or Class attribute
     minbal = 5000
@@ -28,14 +36,21 @@ class SavingsAccount:
         if self.__balance - SavingsAccount.minbal >= amount:
             self.__balance += amount
         else:
-            raise ValueError("Insufficient Balance")
+            raise InsufficientBalanceError(self.__balance)
 
     def getBalance(self):
         return self.__balance
 
 
-SavingsAccount.setMinbal(10000)
-print(SavingsAccount.minbal)
+# SavingsAccount.setMinbal(10000)
+# print(SavingsAccount.minbal)
 a1 = SavingsAccount(1, "Steve", 10000)
-print(a1.__dict__)
-print("Balance", a1.getBalance())
+try:
+    a1.withdraw(20000)
+except InsufficientBalanceError as ex:
+    print(ex)
+    print(ex.balance)
+
+
+# print(a1.__dict__)
+# print("Balance", a1.getBalance())
